c186b86cb625ee9992b1c4d43a88acd1ccf00193
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-20 22:09:49 -0500

    adding input of emojis and search

--- src/components/buscador/InputNewText.tsx
+++ src/components/buscador/InputNewText.tsx
@@ -1,16 +1,27 @@
 import "./styles/inputnewtext.css"
 import { IonIcon } from '@ionic/react';
+import { useState } from "react";
 export default function InputNewText() {
-  
+  const [activeEmojis, setActiveEmojis] = useState<Boolean>(false);
+
   return(
     <div className="containerChat_User_Container_footer">
+      {activeEmojis && <div className="containerChat_User_Container_footer_emojis">
+        <div className="containerChat_User_Container_footer_emojis_input">
+          <input type="text" placeholder="Search your emoji"/>
+        </div>
+        <div className="containerChat_User_Container_footer_emojis_content">
+          
+        </div>
+      </div>}
       <IonIcon icon="add-outline" className="icon_add"></IonIcon>
       <input
           type="text"
           className="containerChat_User_Container_footer_input"
       />
-      <IonIcon icon="happy-outline"></IonIcon>
+      <IonIcon icon="happy-outline" onClick={()=>setActiveEmojis(!activeEmojis)}></IonIcon>
       <IonIcon icon="send-outline"></IonIcon>
+
     </div>
   )
 };
