79fcdf29b42447b9a7cf98f4e8daba0825ef5c48
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-20 20:48:39 -0500

    adding socket io with types

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello, TypeScript!');
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+httpServer.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+io.on('connection', (socket: Socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg: string) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
