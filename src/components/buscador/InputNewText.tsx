6a0363383d301f56fc4a5e2f7befdbdcffef8bc0
EdwardMelendezM <106676335+EdwardMelendezM@users.noreply.github.com>
2023-03-22 22:32:20 -0500

    adding emojis with components popular

--- src/components/buscador/InputNewText.tsx
+++ src/components/buscador/InputNewText.tsx
@@ -1,25 +1,38 @@
 import "./styles/inputnewtext.css"
 import { IonIcon } from '@ionic/react';
-import { useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
+import EmojiPicker from "emoji-picker-react";
+interface EmojiString{
+  emoji:string;
+}
 export default function InputNewText() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [formInput, setFormInput] = useState<string>("");
   const [activeEmojis, setActiveEmojis] = useState<Boolean>(false);
-
-  return(
+  const handleClickOpenEmojis=()=>{
+    setActiveEmojis(!activeEmojis) 
+  }
+  const handleEmojiGetClick =({emoji}:EmojiString)=>{
+    setFormInput(formInput=>formInput+emoji)
+    inputRef.current && (inputRef.current.focus());
+  }
+  const handleChangeInputForm = (e:ChangeEvent<HTMLInputElement>) => {
+    setFormInput(e.target.value);
+  }
+  return( 
     <div className="containerChat_User_Container_footer">
       {activeEmojis && <div className="containerChat_User_Container_footer_emojis">
-        <div className="containerChat_User_Container_footer_emojis_input">
-          <input type="text" placeholder="Search your emoji"/>
-        </div>
-        <div className="containerChat_User_Container_footer_emojis_content">
-          
-        </div>
+        <EmojiPicker  onEmojiClick={handleEmojiGetClick}/>
       </div>}
       <IonIcon icon="add-outline" className="icon_add"></IonIcon>
       <input
           type="text"
           className="containerChat_User_Container_footer_input"
+          ref={inputRef}
+          value={formInput}
+          onChange={handleChangeInputForm}
       />
-      <IonIcon icon="happy-outline" onClick={()=>setActiveEmojis(!activeEmojis)}></IonIcon>
+      <IonIcon icon="happy-outline" onClick={handleClickOpenEmojis}></IonIcon>
       <IonIcon icon="send-outline"></IonIcon>
 
     </div>
